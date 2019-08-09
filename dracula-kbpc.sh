c8d874fd8736376655078d1a1c4805946b820c47
chaoselephant23 <913489+chaoselephant23@users.noreply.github.com>
2019-08-08 20:44:23 -0400

    added git bash script to dracula all putty sessions on my work desktop.

---
+++ dracula-kbpc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+script_dir=$(pwd)
+
+bash "$script_dir/dracula.sh" \
+    admin@imac \
+    git@github.com \
+    jenkins@allegro \
+    jenkins@bvmwin10 \
+    jenkins@imac \
+    jenkins@tvmwin10 \
+    jenkins@tvmwin7 \
+    jenkins@tvmwinsvr2008r2 \
+    jenkins@tvmwinsvr2012r2 \
+    jenkins@tvmwinsvr2016 \
+    kris@allegro \
+    kris@git \
+    kris@www \
+    krisadmin@imac \
+    rds@adagio \
+    rds@allegro \
+    rds@biorad \
+    rds@ci \
+    rds@demo \
+    rds@git \
+    rds@kbb-mysql \
+    rds@linuxmary \
+    rds@mailhost \
+    rds@mariadb \
+    rds@sr1445 \
+    rds@sr1445_pdrds-244 \
+    rds@tvmcentos7 \
+    rds@www \
+    root@allegro \
+    root@binrepo \
+    root@concerto \
+    root@kbb-mysql \
+    root@opus \
+    root@salsa \
+    root@sonata \
+    root@waltz
